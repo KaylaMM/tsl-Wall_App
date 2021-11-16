7e56312e32a15b60266caad6fed4599d4eb91118
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-11-16 17:12:26 -0500

    TW-2 Sign Up functionality and temp HTML files created and tested

--- the_wall/views.py
+++ the_wall/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -7,6 +9,25 @@ def home(request):
     return render(request, "the_wall/index.html")
 
 def signup(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        firstname = request.POST['first-name']
+        lastname = request.POST['last-name']
+        email = request.POST['email']
+        pass1 = request.POST['password']
+        pass2 = request.POST['confirm-pass']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = firstname 
+        myuser.last_name = lastname 
+
+        myuser.save()
+
+        messages.success(request, "Your account has been created!")
+
+        return redirect('login')
+
     return render(request, "the_wall/signup.html")
 
 def login(request):
