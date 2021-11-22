fb5d9e7d65bf54a9ead8c044cbcc0bcf44c89709
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-11-21 23:57:45 -0500

    TW-5 ticket 3/4 complete. New ticket entered for bugs. Bugs need to be fixed to test email functionality

--- the_wall/views.py
+++ the_wall/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,  logout
-
+from tsl_assessment import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoded import force_bytes, force_text 
+from . tokens import generate_token
 
 # Create your views here.
 def home(request): 
@@ -19,13 +24,62 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists! Please try another username.")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 characters")
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords don't match!")
+
+        if not username.isa1num():
+            messages.error(request, "Username must be alpha-numeric")
+            return redirect('home')
+
         myuser = User.objects.create_user (username, email, pass1)
         myuser.firstname = firstname 
         myuser.lastname = lastname 
+        myuser.is_active = False
+        myuser.save() 
+    
 
-        myuser.save()
+        messages.success(request, "Your account has been created! We have sent you a confirmation email to confirm your identity.")
 
-        messages.success(request, "Your account has been created!")
+
+ 
+        #Welcome email 
+
+        subject = "Welcome to The Wall"
+        message = "Hello" + myuser.first_name + "! \n" + "Welcome to The Wall app. \n We've sent you a confirmation email to verify your identity and activate your account \n\n Thank you!." 
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        #Email Address Confirmation Email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @ The Wall Login"
+        message2 = render_to_string('email_confirmation.html', {dict}, {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': (force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser),
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+  
 
         return redirect('signin')
 
@@ -54,3 +108,20 @@ def signout(request):
     logout(request)
     messages.success(request, "Sign Out success")
     return redirect('home') 
+
+
+def activate(request, uidb64, token):
+    try: 
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_Active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render_to_string(request, 'activation_failed.html')
+
+
