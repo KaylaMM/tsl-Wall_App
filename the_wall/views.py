523d496682f3ccb166c29be20c1d8c7e2d8df84c
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-11-30 23:06:06 -0500

    TW-5 and TW-6 complete. Log in works properly, but email activation link must be clicked in order for you to log back in after sign up. Linting issue resolves with lint package installed and settings on VS code

--- the_wall/views.py
+++ the_wall/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.core.mail import EmailMessage, send_mail
-from django.contrib.auth import authenticate, login,  logout
 from tsl_assessment import settings
-from django.core.mail import send_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_text 
+from django.contrib.auth import authenticate, login,  logout
 from . tokens import generate_token
+# from django.core.mail import send_mail
 
 # Create your views here.
 def home(request): 
@@ -34,8 +34,9 @@ def signup(request):
             messages.error(request, "Email already registered!")
             return redirect('home')
 
-        if len(username)>10:
-            messages.error(request, "Username must be under 10 characters")
+        if len(username)>20:
+            messages.error(request, "Username must be under 20 characters")
+            return redirect('home')
 
         if pass1 != pass2:
             messages.error(request, "Passwords don't match!")
@@ -50,8 +51,6 @@ def signup(request):
         myuser.lastname = lastname 
         myuser.is_active = False
         myuser.save() 
-    
-
         messages.success(request, "Your account has been created! We have sent you a confirmation email to confirm your identity.")
 
 
@@ -65,13 +64,13 @@ def signup(request):
         send_mail(subject, message, from_email, to_list, fail_silently=True)
 
         #Email Address Confirmation Email
-
         current_site = get_current_site(request)
         email_subject = "Confirm your email @ The Wall Login"
         message2 = render_to_string('email_confirmation.html', {
+
             'name': myuser.first_name,
             'domain': current_site.domain,
-            'uid': (force_bytes(myuser.pk)),
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
             'token': generate_token.make_token(myuser),
         })
         email = EmailMessage(
@@ -83,8 +82,8 @@ def signup(request):
         email.fail_silently = True
         email.send()
   
-
         return redirect('signin')
+
 
     return render(request, "the_wall/signup.html")
 
@@ -92,16 +91,17 @@ def activate(request, uidb64, token):
     try: 
         uid = force_text(urlsafe_base64_decode(uidb64))
         myuser = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+    except (TypeError,ValueError,OverflowError, User.DoesNotExist):
         myuser = None
 
     if myuser is not None and generate_token.check_token(myuser, token):
-        myuser.is_Active = True
+        myuser.is_active = True
         myuser.save()
         login(request, myuser)
+        messages.success(request, "Your Account has been activated!!")
         return redirect('signin')
     else:
-        return render_to_string(request, 'activation_failed.html')
+        return render(request, 'activation_failed.html')
 
 def signin(request):
 
@@ -114,7 +114,7 @@ def signin(request):
         if user is not None:
             login(request, user)
             firstname = user.first_name 
-            return render(request, 'the _wall/index.html', {'firstname': firstname})
+            return render(request, 'the_wall/index.html', {'firstname': firstname})
 
         else:
             messages.error(request, "Uh oh! Your username/password is incorrect.")
